07f87c790d6349fbab8350ff3ec8bdd597edb5ba
Samuel Byland <samuel.martin.byland@cern.ch>
2016-03-18 19:56:58 +0100

    Corrected a few errors

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+// First test with git
+// I don't know what I should do …
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!\n");
+  printf("Some more text\n");
+  
+  for (int i=0;i<10;i++) {
+    printf("Counting %i\n",i);
+  }
+  
+}
