413b5d78e65bb0118386c0a2086d8b91f14a6be1
Samuel Byland <samuel.martin.byland@cern.ch>
2016-03-22 07:44:09 +0100

    Added some new files.

---
+++ helloworld.py
@@ -0,0 +1,25 @@
+print "Hello world!"
+
+x = 2
+if x==1:
+    print "x is 1"
+
+my_name = 'Samuel'
+
+print "My name is %s and I'm a physicist." % my_name    
+print "I have %d cars." % x
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+print "old filename: ",
+fn_old = raw_input()
+
+print "new filename: ",
+fn_new = raw_input()
+
+print "old filename %s and new filename %s" % (fn_old, fn_new)
+
+
